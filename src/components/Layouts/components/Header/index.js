470f7383223e133ced0c66ad9e40da3647514ff0
Blue Moon <s2bluemoon01@gmail.com>
2022-08-01 14:56:33 +0700

    Dựng khung Layout mặc định

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// để đặt tên classnames có dấu - thì phải dùng bind
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return <header className={cx('wrapper')} >
+        <div className={cx('inner')}></div>
+    </header>;
 }
 
 export default Header;
