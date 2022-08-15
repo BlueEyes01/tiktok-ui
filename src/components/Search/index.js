7505e393f1bc0d2203e7e1a3e8c7e2e31b811f32
Blue Moon <s2bluemoon01@gmail.com>
2022-08-15 16:56:11 +0700

    Xây dựng Logic phần Header #1

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -38,7 +38,7 @@ function Search() {
             interactive
             visible={showResult && searchResult.length > 0}
             render={(attrs) => (
-                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                <div className={cx('search-results')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
                         <AccountItem />
